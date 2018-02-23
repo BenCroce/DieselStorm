16a67443506147352b3e176c940d587a22fdefb3
Matthew Williamson <chuwilliamson@gmail.com>
2018-02-23 11:59:50 -0600

    add cinemachine, terrain scene for ben, and a test script for checking gitignore

---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviour : MonoBehaviour {
+    public int TestNum;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(TestNum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
