1378ccbf30782ecabc34b9c45445f8bf18feba2d
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-02-23 18:09:28 -0600

    Hoover Tank

---
+++ Assets/Scripts/Behaviours/LightTankMovementBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class LightTankMovementBehaviour : MonoBehaviour
+{
+    public LightTankScriptable m_LightTankScriptable;
+    public Rigidbody m_Rigidbody;
+    public float m_MinToGround;
+    public float d2f;
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {   
+        RaycastHit hit;
+        if (Physics.BoxCast(transform.position, transform.lossyScale, -transform.up, out hit))
+        {
+             d2f = Vector3.Distance(transform.position, hit.point);
+        }        
+        if (d2f < m_MinToGround)
+            m_Rigidbody.AddForce(m_LightTankScriptable.m_ThrusterForce);
+    }    
+}
---
+++ Assets/Scripts/ScriptableObjects/LightTankScriptable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightTankScriptable", menuName = "Tanks/LightTank")]
+public class LightTankScriptable : ScriptableObject
+{
+    public Vector3 m_ThrusterForce;
+    public float m_MaxForwardMovementSpeed;
+    public float m_MaxHorizontalMovementSpeed;
+}
