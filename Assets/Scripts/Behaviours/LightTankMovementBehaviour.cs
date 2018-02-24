8b5d276dc1063f2ebb02ff611f54d8dd338e47ed
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-02-24 10:48:13 -0600

    LightTankMovement
    Tank is hoovering now. Trying to get the hoover to work well with
    terrian elevation changes

--- Assets/Scripts/Behaviours/LightTankMovementBehaviour.cs
+++ Assets/Scripts/Behaviours/LightTankMovementBehaviour.cs
@@ -7,21 +7,46 @@ public class LightTankMovementBehaviour : MonoBehaviour
 {
     public LightTankScriptable m_LightTankScriptable;
     public Rigidbody m_Rigidbody;
-    public float m_MinToGround;
-    public float d2f;
+    public float m_HooverHeight;
+    public List<Vector3> m_ThrusterOffset;
+    public List<Vector3> m_ThrusterLocations;
+
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+
     }
 
     void FixedUpdate()
-    {   
-        RaycastHit hit;
-        if (Physics.BoxCast(transform.position, transform.lossyScale, -transform.up, out hit))
+    {
+        for (int i = 0; i < 4; i++)
         {
-             d2f = Vector3.Distance(transform.position, hit.point);
-        }        
-        if (d2f < m_MinToGround)
-            m_Rigidbody.AddForce(m_LightTankScriptable.m_ThrusterForce);
-    }    
+            m_ThrusterLocations[i] = transform.position + m_ThrusterOffset[i];
+            Ray ray = new Ray(m_ThrusterLocations[i], -transform.up);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, m_HooverHeight))
+            {
+                float d2f = (m_HooverHeight - hit.distance) / m_HooverHeight;
+                Vector3 appliedHooverForce = Vector3.up * d2f *
+                                             m_LightTankScriptable.m_HooverForce;
+                m_Rigidbody.AddForceAtPosition(appliedHooverForce, m_ThrusterLocations[i],
+                    ForceMode.Acceleration);
+                Quaternion newRotation = new Quaternion(hit.transform.rotation.x,
+                    transform.rotation.y, hit.transform.rotation.z, transform.rotation.w);
+                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation,
+                    Time.deltaTime);
+            }
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (var thruster in m_ThrusterLocations)
+        {
+            Gizmos.DrawWireSphere(thruster, .5f);
+        }
+    }
+
 }
--- Assets/Scripts/ScriptableObjects/LightTankScriptable.cs
+++ Assets/Scripts/ScriptableObjects/LightTankScriptable.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LightTankScriptable", menuName = "Tanks/LightTank")]
 public class LightTankScriptable : ScriptableObject
 {
-    public Vector3 m_ThrusterForce;
-    public float m_MaxForwardMovementSpeed;
-    public float m_MaxHorizontalMovementSpeed;
+    public float m_HooverForce;
+    public float m_StrafeForce;
+    public float m_ForwardMovementSpeed;
+    public float m_TurnSpeed;
 }
