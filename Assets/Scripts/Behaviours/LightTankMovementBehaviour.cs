02ccdb6e8e843de257c9caee9b3acc308e1f8b26
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-02-25 11:41:55 -0600

    Tank Hoover with one thruster

--- Assets/Scripts/Behaviours/LightTankMovementBehaviour.cs
+++ Assets/Scripts/Behaviours/LightTankMovementBehaviour.cs
@@ -8,45 +8,34 @@ public class LightTankMovementBehaviour : MonoBehaviour
     public LightTankScriptable m_LightTankScriptable;
     public Rigidbody m_Rigidbody;
     public float m_HooverHeight;
-    public List<Vector3> m_ThrusterOffset;
-    public List<Vector3> m_ThrusterLocations;
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
-
     }
 
     void FixedUpdate()
     {
-        for (int i = 0; i < 4; i++)
-        {
-            m_ThrusterLocations[i] = transform.position + m_ThrusterOffset[i];
-            Ray ray = new Ray(m_ThrusterLocations[i], -transform.up);
+            Ray ray = new Ray(transform.position, -transform.up);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, m_HooverHeight))
-            {
-                float d2f = (m_HooverHeight - hit.distance) / m_HooverHeight;
-                Vector3 appliedHooverForce = Vector3.up * d2f *
-                                             m_LightTankScriptable.m_HooverForce;
-                m_Rigidbody.AddForceAtPosition(appliedHooverForce, m_ThrusterLocations[i],
-                    ForceMode.Acceleration);
-                Quaternion newRotation = new Quaternion(hit.transform.rotation.x,
-                    transform.rotation.y, hit.transform.rotation.z, transform.rotation.w);
-                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation,
-                    Time.deltaTime);
-            }
+        if (Physics.Raycast(ray, out hit, m_HooverHeight))
+        {
+            float d2f = (m_HooverHeight - hit.distance);
+            Vector3 appliedHooverForce = Vector3.up * d2f *
+                                         m_LightTankScriptable.m_HooverForce;
+            m_Rigidbody.AddForceAtPosition(appliedHooverForce, transform.position,
+                ForceMode.Acceleration);
+            Quaternion newRotation = new Quaternion(hit.transform.rotation.x,
+                transform.rotation.y, hit.transform.rotation.z, transform.rotation.w);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation,
+                Time.deltaTime);
+            m_Rigidbody.AddForce(transform.forward * 0.1f);
         }
 
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        foreach (var thruster in m_ThrusterLocations)
-        {
-            Gizmos.DrawWireSphere(thruster, .5f);
-        }
     }
 
 }
