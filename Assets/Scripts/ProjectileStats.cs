339bbb5acf7e9652b5384c9595a166b5de6b207e
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-03-02 16:56:39 -0600

    All the stats

---
+++ Assets/Scripts/ProjectileStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileStats : MonoBehaviour
+{
+    [SerializeField]
+    private List<StatScriptable> m_stats;
+
+    [SerializeField]
+    private List<ModifierScriptable> m_Mods;
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (var mod in m_Mods)
+        {
+            if (other.GetComponent<TankStats>() != null)
+            {
+                if (other.GetComponent<TankStats>().HasStat(mod))
+                {
+                    mod.NegativeMod();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/TankStats.cs
+++ Assets/Scripts/TankStats.cs
@@ -7,5 +7,13 @@ public class TankStats : MonoBehaviour
     [SerializeField]
     private List<StatScriptable> m_Stats;
 
-
+    public bool HasStat(ModifierScriptable mod)
+    {
+        foreach (var stat in m_Stats)
+        {
+            if (stat == mod.m_Stat)
+                return true;
+        }
+        return false;
+    }
 }
