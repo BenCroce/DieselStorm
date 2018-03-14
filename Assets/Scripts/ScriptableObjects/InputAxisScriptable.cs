6b1600a914c18f0ad92c04c7e276dcafc3a22a7f
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-03-14 16:30:04 -0500

    Input Scriptables

---
+++ Assets/Scripts/ScriptableObjects/InputAxisScriptable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Input Axis", menuName = "Input Axis")]
+public class InputAxisScriptable : ScriptableObject
+{
+    public string m_AxisName;    
+
+    public float AxisValue()
+    {        
+        return Input.GetAxis(m_AxisName);
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/InputButtonScriptable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Input Button", menuName = "Input Button")]
+public class InputButtonScriptable : ScriptableObject {
+
+    public string m_ButtonName;
+
+    public bool IsButtonDown()
+    {
+        return Input.GetButton(m_ButtonName);
+    }
+
+    public bool WasButtonPressed()
+    {
+        return Input.GetButtonDown(m_ButtonName);
+    }
+
+    public bool WasButtonReleased()
+    {
+        return Input.GetButtonUp(m_ButtonName);
+    }
+}
