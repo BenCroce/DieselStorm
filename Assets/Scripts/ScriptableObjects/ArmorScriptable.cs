01bf67e443eb00146bc36a6113ef71216a532d93
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-03-16 16:41:17 -0500

    Making progress on mitigation

--- Assets/Scripts/ScriptableObjects/ArmorScriptable.cs
+++ Assets/Scripts/ScriptableObjects/ArmorScriptable.cs
@@ -6,5 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ArmorStat", menuName = "ArmorScriptable")]
 public class ArmorScriptable : StatScriptable
 {
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_Mitigation;
 
+    public float Mitigation { get { return m_Mitigation; } }
 }
