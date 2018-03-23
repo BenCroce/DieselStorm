ea924d6fdc9273c5f42c4cce802acaf61af69714
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-03-23 14:49:17 -0500

    Stats for mag

---
+++ Assets/Scripts/Behaviours/MagazineBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagazineBehaviour : MonoBehaviour
+{
+    public MagaizeScriptable m_MagazineConfig;
+    public MagaizeScriptable m_rtMagazine;
+
+
+    void Awake()
+    {
+        m_rtMagazine = m_MagazineConfig.CreateInstance();
+    }
+
+    void UseRound()
+    {
+        m_rtMagazine.ConsumeAmmo();
+    }
+
+    void ReloadMag()
+    {
+        m_rtMagazine.ReloadAmmo();
+    }
+}
