fd6e0db37c6bf6ab4bf9dfdf2dbad0b93ed2f1cf
ZyronMelancon <altausolaris@gmail.com>
2018-03-29 17:55:37 -0500

    Ported old lighttankmovement scripts to new setup

---
+++ Assets/Scripts/ScriptableObjects/LightTankHoverScriptable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightTankScriptable", menuName = "Tanks/LightHoverTank")]
+public class LightTankHoverScriptable : LightTankScriptable
+{
+    public float m_HooverHeight;
+}
