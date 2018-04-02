6f42007563de078ca75c9c3d8292a216010af00e
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-04-02 15:16:13 -0500

    Import Lobby Manager

---
+++ Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerBehaviour : NetworkBehaviour
+{
+    [SyncVar] public string m_Name;
+    [SyncVar] public int m_Score;
+    [SyncVar] public Color m_TeamColor;
+
+    public void SetTeamColor(UnityEngine.Object[] obj)
+    {
+        var sender = obj[0] as TeamBehaviour;
+        var player = obj[1] as PlayerBehaviour;
+
+        if (player == this)
+        {                          
+            m_TeamColor = (sender.Color == null) ?  Color.gray : sender.Color;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Behaviours/TeamBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TeamBehaviour : NetworkBehaviour
+{
+    public List<PlayerBehaviour> m_Players;
+    public GameEventArgs m_OnPlayerAdded;
+    [SerializeField]
+    private Color m_Color;
+
+    public Color Color
+    {
+        get { return m_Color; }
+    }
+
+    public void AddPlayer(PlayerBehaviour player)
+    {
+        if(m_Players.Contains(player))
+            return;
+        m_Players.Add(player);
+        m_OnPlayerAdded.Raise(this, player);        
+    }
+}
