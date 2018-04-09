c12337cabd727713e43085599a1a30cd9b10cd13
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-04-09 18:01:40 -0500

    Network Lobby Hook

---
+++ Assets/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Prototype.NetworkLobby;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook : LobbyHook
+{
+    public GameEventArgs m_OnPlayerConnected;    
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager,
+        GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        m_OnPlayerConnected.Raise(this, lobbyPlayer, gamePlayer);
+    }
+}
