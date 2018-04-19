39c31b4a3297809085607d12a820cb63cff08feb
ZyronMelancon <altausolaris@gmail.com>
2018-04-19 16:30:21 -0500

    More camera work

--- Assets/Scripts/CameraSteerguide.cs
+++ Assets/Scripts/CameraSteerguide.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CinemachineSteerguide : MonoBehaviour {
+public class CameraSteerguide : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate ()
