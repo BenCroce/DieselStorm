1d4f44cedd31fa3b06016185fa60763062c75b73
ZyronMelancon <altausolaris@gmail.com>
2018-04-24 18:00:01 -0500

    camera tweaks + ui

---
+++ Assets/Scripts/TurrentAimBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrentAimBehaviour : MonoBehaviour {
+
+    public Transform m_aimGuide;
+    public Transform m_verticalAxis;
+    public Transform m_horizontalAxis;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+}
