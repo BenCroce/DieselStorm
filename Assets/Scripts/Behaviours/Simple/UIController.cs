a17072915f1b79769b5cf91e6bf86c9580590168
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-05-15 16:34:37 -0500

    Lobby is working

--- Assets/Scripts/Behaviours/Simple/UIController.cs
+++ Assets/Scripts/Behaviours/Simple/UIController.cs
@@ -8,11 +8,27 @@ public class UIController : MonoBehaviour
     public GameObject m_MainMenu;
     public GameObject m_CreditsMenu;
     public GameObject m_ServerMenu;
+    public GameObject m_GameMenu;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     public void TransitionMenu(GameObject menu)
     {
         menu.SetActive(true);
         m_ActiveWindow.SetActive(false);
         m_ActiveWindow = menu;
-    }    
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
 }
