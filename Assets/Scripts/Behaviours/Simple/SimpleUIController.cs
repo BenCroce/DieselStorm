ce951ea294cd0fb9f9a4fed88b5eb8380902a46a
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-05-18 00:23:33 -0500

    Setting up for tank selection

--- Assets/Scripts/Behaviours/Simple/SimpleUIController.cs
+++ Assets/Scripts/Behaviours/Simple/SimpleUIController.cs
@@ -7,13 +7,25 @@ using UnityEngine.UI;
 public class SimpleUIController : NetworkBehaviour
 {
     public GameObject m_MathEndPanel;
+    public GameObject m_TankSelection;
     public Text m_MatchEndText;
-    public string m_Textdata;    
+    public string m_Textdata;
     public Color m_TextColor;
+    public SimplePlayerBehaviour m_RespawningPlayer;    
 
     [ClientRpc]
     public void RpcDisplayMatchEndUI(bool status)
     {
         m_MathEndPanel.SetActive(status);
-    }   
+    }
+
+    public void OnPlayerDied(Object[] args)
+    {
+        m_TankSelection.SetActive(true);
+    }
+
+    public void TankSelected(GameObject tankPrefab)
+    {
+        m_RespawningPlayer.m_TankObjectPrefab = tankPrefab;
+    }
 }
