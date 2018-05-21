a72c8753e56843d840bbf5b7ab4f6d338ce15d2f
ZyronMelancon <altausolaris@gmail.com>
2018-05-21 15:42:30 -0500

    Added explosionbehaviour

---
+++ Assets/Scripts/Behaviours/ExplosionBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ProjectileStats))]
+public class ExplosionBehaviour : MonoBehaviour {
+
+    public ShellBehaviour m_shell;
+
+    ProjectileStats m_stats;
+
+    [SerializeField]
+    ModifierScriptable rt_damage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_stats = GetComponent<ProjectileStats>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (transform.parent.gameObject == other.gameObject)
+            return;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_shell.m_explosionRadius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            TankStats targetTankStats;
+
+            if (colliders[i].GetComponent<TankStats>())
+                targetTankStats = colliders[i].GetComponent<TankStats>();
+            else
+                continue;
+
+            if (colliders[i] != other)
+            {
+                rt_damage = Instantiate(m_stats.m_HealthModifier) as ModifierScriptable;
+                rt_damage.m_Value = (int)(rt_damage.m_Value * ((m_shell.m_explosionRadius - Vector3.Magnitude(transform.position - colliders[i].transform.position)) / m_shell.m_explosionRadius));
+                m_stats.m_ProjectileHit.Raise(this.gameObject, rt_damage, colliders[i].gameObject);
+            }
+        }
+    }
+}
